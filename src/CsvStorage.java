357729bbc514156035a1c0a0ac1a7ee10ccf7e14
LukasAnell <lukasmanell@gmail.com>
2026-05-07 03:14:19 -0400

    First implementation of loadTable()

--- src/CsvStorage.java
+++ src/CsvStorage.java
@@ -38,7 +38,67 @@ public class CsvStorage {
 		}
 	}
 
-	public static Table loadTable(String tableName, String filePath) {
+	public static Table loadTable(String tableName, String filePath) throws IOException {
+		Table table;
+		List<Column> columns = new ArrayList<>();
 
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+			// read in only column headers,
+			// then parse and initialize table object
+			String line = reader.readLine();
+			
+			String[] columnStrings = line.split(",");
+			for (String col : columnStrings) {
+				String[] sep = col.split(":");
+				
+				String name = sep[0];
+				DataType type = DataType.valueOf(sep[1]);
+
+				columns.add(new Column(name, type));
+			}
+
+			// initialize table with new column data
+			table = new Table(tableName, columns);
+
+			// loop through remaining rows
+			while ((line = reader.readLine()) != null) {
+				// construct next row object, append to table
+				String[] sep = line.split(",");
+
+				List<Object> values = new ArrayList<>();
+				// go through cols and convert to data type before appending to current row object
+				for (int i = 0; i < sep.length; i++) {
+					DataType type = columns.get(i).getType();
+					
+					String value = sep[i];
+
+					// convert value to be type
+					Object convertedValue = null;
+					switch (type) {
+						case STRING:
+							convertedValue = value;
+							break;
+						case INTEGER:
+							convertedValue = Integer.parseInt(value);
+							break;
+						case DOUBLE:
+							convertedValue = Double.parseDouble(value);
+							break;
+						case BOOLEAN:
+							convertedValue = Boolean.parseBoolean(value);
+							break;
+						default:
+							convertedValue = value;
+					}
+
+					values.add(convertedValue);
+				}
+
+				// make a Row object out of values, then add to table
+				table.addRow(new Row(values));
+			}
+		} catch (IOException e) {
+			throw e;
+		}
 	}
 }
