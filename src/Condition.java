ba4a33db50b8ba00b599d61ab561b91ce463e92a
LukasAnell <lukasmanell@gmail.com>
2026-05-07 12:51:00 -0400

    Implemented classes for Condition and QueryResult

---
+++ src/Condition.java
@@ -0,0 +1,23 @@
+public class Condition {
+	private String columnName;
+	private String operator;
+	private String value;
+
+	public Condition(String columnName, String operator, String value) {
+		this.columnName = columnName;
+		this.operator = operator;
+		this.value = value;
+	}
+
+	public String getColumnName() {
+		return columnName;
+	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
---
+++ src/QueryResult.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class QueryResult {
+	private List<Row> rows;
+	private String message;
+
+	public QueryResult(List<Row> rows, String message) {
+		this.rows = rows;
+		this.message = message;
+	}
+
+	public List<Row> getRows() {
+		return rows;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
