f96998a769041b0d10a08b80b570414af8cb222a
LukasAnell <lukasmanell@gmail.com>
2026-05-08 03:10:11 -0400

    First implementation of HashIndex

--- src/HashIndex.java
+++ src/HashIndex.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,21 +8,35 @@ public class HashIndex {
 
 	public HashIndex(String columnName, Table table) {
 		this.columnName = columnName;
+
 		// build index from table's rows
+		List<Row> rows = table.getRows();
+		for (Row row : rows) {
+			// for each row, loop through values and add to HashMap
+			for (int i = 0; i < table.getColumns().size(); i++) {
+				Object key = row.getValue(i);
+
+				// if value doesn't exist yet, create a new List
+				// otherwise, append row onto existing List
+				index.computeIfAbsent(key, k -> new ArrayList<>()).add(row);
+			}
+		}
 	}
 
 	public List<Row> lookup(Object key) {
-		//
-
-		return null;
+		return index.get(key);
 	}
 
 	public void insert(Object key, Row row) {
-
+		index.computeIfAbsent(key, k -> new ArrayList<>()).add(row);
 	}
 
 	public void remove(Object key, Row row) {
+		List<Row> value = index.get(key);
 
+		value.remove(row);
+
+		index.put(key, value);
 	}
 
 	public String getColumnName() {
