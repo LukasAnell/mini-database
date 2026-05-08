4516f15b9603b95234b398aa6709d0c694a610da
LukasAnell <lukasmanell@gmail.com>
2026-05-08 00:32:05 -0400

    Add helper methods for parsing WHERE condition and testing a row against a Condition

--- src/QueryParser.java
+++ src/QueryParser.java
@@ -30,4 +30,69 @@ public class QueryParser {
 
 		return null;
 	}
+
+	private Condition getWhereCondition(String query) {
+		// condition structure: colName op value
+		
+		// get substring of only condition
+		String conditionStr = query.substring(query.toUpperCase().indexOf("WHERE") + 6);
+
+		// check which operator there is
+		String operator;
+		if (conditionStr.contains("+")) {
+			operator = "+";
+		} else if (conditionStr.contains(">")) {
+			operator = ">";
+		} else {
+			operator = "<";
+		}
+
+		// get colName and value
+		String[] values = conditionStr.split(operator);
+
+		return new Condition(values[0].strip(), operator, values[1].strip());
+	}
+
+	private boolean testRow(Row row, Condition condition, List<Column> columns) {
+		// find which column index matches condition's colName
+		int index = -1;
+		for (int i = 0; i < columns.size(); i++) {
+			if (condition.getColumnName().equals(columns.get(i).getName())) {
+				index = i;
+				break;
+			}
+		}
+		
+		// get value from row at index
+		Object rowValue = row.getValue(index);
+		
+		// cast condition's value to correct type based on column's DataType
+		Class<?> targetType = columns.get(index).getType().getClass();
+		Object conditionValueCasted = targetType.cast(condition.getValue());
+
+		// check if rowValue and conditionValueCasted can be Compared
+		if (!(rowValue instanceof Comparable && conditionValueCasted instanceof Comparable)) {
+			return false;
+		}
+
+		// create Comparable version of rowValue, to be used when comparing with =, >, <
+		@SuppressWarnings("unchecked")
+		Comparable<Object> c1 = (Comparable<Object>) rowValue;
+
+		
+		// compare using =, >, <
+		// (for STRING and BOOLEAN, only use =)
+		if (targetType == Integer.class || targetType == Double.class) {
+			// compare with >, <
+			switch (condition.getOperator()) {
+				case ">":
+					return c1.compareTo(conditionValueCasted) > 0;
+				case "<":
+					return c1.compareTo(conditionValueCasted) < 0;
+			}
+		}
+
+		// compare with =
+		return c1.compareTo(conditionValueCasted) == 0;
+	}
 }
