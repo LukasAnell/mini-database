1f815e89586bbfc6c51e5311e5d2cabc5048782e
LukasAnell <lukasmanell@gmail.com>
2026-05-11 00:18:00 -0700

    Added public function outlines

---
+++ src/Transaction.java
@@ -0,0 +1,23 @@
+public class Transaction {
+	private Table table;
+
+	public Transaction(Table table) {
+		this.table = table;
+	}
+
+	public void executeQuery(String query, QueryParser parser) {
+
+	}
+
+	public void commit() {
+
+	}
+
+	public void rollback() {
+
+	}
+
+	public boolean isCommitted() {
+
+	}
+}
