a63009817998b3803dd6bc7f608a84a8fe151d65
LukasAnell <lukasmanell@gmail.com>
2026-05-30 14:30:04 -0700

    Javadocs for remaining HashIndex methods

--- src/HashIndex.java
+++ src/HashIndex.java
@@ -36,9 +36,11 @@ public class HashIndex {
     private HashMap<Object, List<Row>> index;
 
     /**
+     * Create a HashIndex for the specified column in the given table.
+     * The index is built from the existing rows in the table.
      *
-     * @param columnName
-     * @param table
+     * @param columnName The name of the column to index
+     * @param table The table from which to build the index
      */
     public HashIndex(String columnName, Table table) {
         this.columnName = columnName;
@@ -61,27 +63,31 @@ public class HashIndex {
     }
 
     /**
+     * Lookup rows in the index that match the given key.
      *
-     * @param key
-     * @return
+     * @param key The value to look up in the indexed column
+     * @return A list of rows that match the given key, or an empty list if no matches are found
      */
     public List<Row> lookup(Object key) {
         return index.getOrDefault(key, new ArrayList<>());
     }
 
     /**
+     * Insert a new row into the index for the given key.
+     * If the key already exists, the row is added to the existing list of rows for that key.
      *
-     * @param key
-     * @param row
+     * @param key The value of the indexed column for the new row
+     * @param row The row to insert into the index
      */
     public void insert(Object key, Row row) {
         index.computeIfAbsent(key, k -> new ArrayList<>()).add(row);
     }
 
     /**
+     * Remove a row from the index for the given key.
      *
-     * @param key
-     * @param row
+     * @param key The value of the indexed column for the row to remove
+     * @param row The row to remove from the index
      */
     public void remove(Object key, Row row) {
         List<Row> value = index.get(key);
@@ -92,8 +98,9 @@ public class HashIndex {
     }
 
     /**
+     * Get the name of the indexed column.
      *
-     * @return
+     * @return the name of the indexed column
      */
     public String getColumnName() {
         return columnName;
